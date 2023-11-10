f7328367e37a239924612adb98d257887d91fb24
miwa6024 <148538465+miwa6024@users.noreply.github.com>
2023-11-10 19:14:22 +0900

    Update 2023,11,10-kadai1.c

--- 2023,11,10-kadai1.c
+++ 2023,11,10-kadai1.c
@@ -4,13 +4,16 @@ int main(void) {
 	int max = 10000;
 
 	for (int i = 1; i <= max; i++) {
-		if (i % 13 == 0) {
-			printf("%d\n",i);
+		if (i % 221 == 0) {
+			printf("    %d\n", i);
+		}
+		else if (i % 13 == 0) {
+			printf("%d\n", i);
 		}
 		else if (i % 17 == 0) {
-			printf("%d\n",i);
+			printf("%d\n", i);
 		}
-		
+
 	}
 
 	return 0;
